4b227f7183637f582ad99594b42bb1df4c5c38ba
moTzxx <JingYin007@users.noreply.github.com>
2017-08-03 17:09:56 +0800

    ok
    qr Code

--- Application/M/Controller/QrCodeController.class.php
+++ Application/M/Controller/QrCodeController.class.php
@@ -10,6 +10,9 @@ use Think\Controller;
 
 class QrCodeController extends Controller
 {
+    public function index(){
+        $this->display();
+    }
     /**
      * 二维码测试效果
      * png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false)
@@ -17,10 +20,24 @@ class QrCodeController extends Controller
     public function qrCode(){
         vendor("phpqrcode.phpqrcode");
         $str = '你若盛开，清风自来';
-        \QRcode::png($str,'qrCodeCoco.jpg',QR_ECLEVEL_H,4,1,true);
+        //\QRcode::png($str,'qrCodeCoco.jpg',QR_ECLEVEL_H,4,1,true);
+        \QRcode::png($str,false,QR_ECLEVEL_H,4,1,true);
     }
     public function qrcode_jquery(){
-        //echo 'ss';
         $this->display();
+    }
+    public function qrCode_vcard(){
+        vendor("phpqrcode.phpqrcode");
+        $content = 'BEGIN:VCARD'."\n";
+        $content.= 'VERSION: 1.3.1'."\n";
+        $content.= 'N: 毛球'."\n";
+        $content.= 'FN: 贝利斯'."\n";
+        $content.= 'TEL;WORK;VOICE: 18898989988'."\n";
+        $content.= 'TEL;HOME;VOICE: 17789889999'."\n";
+        $content.= 'ORG: 阿尔及利亚-安道尔'."\n";
+        $content.= 'URL: http:www.moTou.com'."\n";
+        $content.= 'END:VCARD'."\n";
+
+        \QRcode::png($content,false,QR_ECLEVEL_L,3,2);
     }
 }
