8b2de2530317dd64683f6e2573381f9410efa53d
moTzxx <JingYin007@users.noreply.github.com>
2017-10-09 09:08:51 +0800

    file
    文件目录操作 测试

---
+++ Application/M/Controller/FileController.class.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 百鬼夜行
+ * Date: 2017/10/8
+ * Time: 15:12
+ */
+
+namespace M\Controller;
+
+
+use Think\Controller;
+
+class FileController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+    public function write(){
+        // 打开文件
+        // 将文件的内容读取出来，在开头加入Hello World
+        // 将拼接好的字符串写回到文件当中
+
+        $file = './file_hello.txt';
+        $handle = fopen($file, 'r');
+        $content = fread($handle, filesize($file));
+        $content = 'Hello World'. $content;
+        fclose($handle);
+        $handle = fopen($file, 'w');
+        fwrite($handle, $content);
+        fclose($handle);
+
+        $res = file_get_contents('file_hello.txt');
+        var_dump($res);
+    }
+    public function loopDir($dir){
+
+        $handle = opendir($dir);
+
+
+        while (false !==($file = readdir($handle))){
+            if ($file != '.' && $file != '..'){
+                echo $file."<br/>";
+                if (filetype($dir.'/'.$file) == 'dir'){
+                    $this->loopDir($dir.'/'.$file);
+                }
+            }
+        }
+    }
+
+    /**
+     * 打开目录
+     * 读取目录当中的文件
+     * 如果文件类型是目录，继续打开目录
+     * 读取子目录的文件
+     * 如果文件类型是文件，输出文件名称
+     * 关闭目录
+     */
+    public function dir(){
+        $dir = './test';
+        $this->loopDir($dir);
+    }
+}
---
+++ Application/M/Controller/TestController.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 百鬼夜行
+ * Date: 2017/10/8
+ * Time: 15:12
+ */
+
+namespace M\Controller;
+
+
+class TestController
+{
+    public function __construct()
+    {
+        header("Content-Type:text/html;charset=utf-8");
+    }
+
+    public function index()
+    {
+
+    }
+}
