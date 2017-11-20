45f2972fe265996dce23f69ce01ec79da841b2ab
moTzxx <JingYin007@users.noreply.github.com>
2017-11-20 15:58:47 +0800

    html <map>标签使用
    添加了 图片热点的测试代码 博客可做参考

---
+++ Application/M/Controller/HtmlController.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 百鬼夜行
+ * Date: 2017/10/8
+ * Time: 15:12
+ */
+
+namespace M\Controller;
+
+
+use Think\Controller;
+
+/**
+ * 记录一些前端的 知识
+ * Class HtmlController
+ * @package M\Controller
+ */
+class HtmlController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        header("Content-Type:text/html;charset=utf-8");
+    }
+
+    public function index()
+    {
+        $this->display();
+    }
+
+    /**
+     * 图片热点
+     */
+    public function map()
+    {
+        $this->display();
+    }
+
+
+}
